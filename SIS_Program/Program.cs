6f3bb8320c6835f7ab3699c7dc69a1e20f9a1da6
Glenn Kinstler <glenn@glennkinstler.com>
2018-11-12 10:58:12 -0600

    First 'recommit' to hide connection string

---
+++ SIS_Program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace SIS_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string jsonText = File.ReadAllText("appsettings.development.json");
+            JObject jObject = JObject.Parse(jsonText);
+
+            string connStr = jObject.GetValue("ConnectionString").ToString();
+
+            {
+                Students stuList = new Students();
+
+                List<Students> students = stuList.GetAllStudents();
+
+                CreateWebHostBuilder(args).Build().Run();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>();
+        
+    }
+}
---
+++ SIS_Program/Students.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SIS_Program
+{
+    public class Students
+    {
+        public static string ConnectionString { get; set; }
+        public int StudentId { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public List<Students> GetAllStudents()
+        {
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+            List<Students> students = new List<Students>();
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM students;";
+
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Students student = new Students()
+                    {
+                        StudentId = (int)dataReader["StudentID"],
+                        FirstName = dataReader["firstname"].ToString(),
+                        LastName = dataReader["lastname"].ToString(),
+                    };
+
+                    students.Add(student);
+                }
+
+                return students;
+            }
+        }
+    }
+}
